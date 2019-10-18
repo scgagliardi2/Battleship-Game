c27d0826993c79ad4633d342365c079bee8872c7
Samuel C Gagliardi <scgagliardi2@gmail.com>
2019-10-18 14:41:23 -0500

    input updates

--- Game.py
+++ Game.py
@@ -16,10 +16,19 @@ print("Welcome to Battleship, here is your board!")
 human_personal_board.print_board()
 
 # generate human ships
-carrier_coordinate = input("Where do you want to start your carrier? ")
-carrier_direction = input(
+while True:
+    carrier_coordinate = input("Where do you want to start your carrier? ")
+    if human_player.check_input(carrier_coordinate):
+        carrier_coordinate = carrier_coordinate.upper()
+        break
+while True:
+    carrier_direction = input(
     "What direction is it facing? Up, down, left, or right? ")
+    if human_player.check_direction(carrier_direction):
+        carrier_direction = carrier_direction.casefold()
+        break
 human_carrier = Ship.Ship(5, "carrier", carrier_coordinate, carrier_direction)
 human_personal_board.place_ship(human_carrier)
 
+# Carrier (5), Battleship (4), Cruiser (3), Submarine (3), and Destroyer (2).
 human_personal_board.print_board()
--- Player.py
+++ Player.py
@@ -5,3 +5,26 @@ class Player:
 
     def guess(self, coordinate, board):
         pass
+    
+    def check_input(self, string):
+        letter_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        number_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"] 
+        for letter in letter_list:
+            for number in number_list:
+               coordinate = letter + number
+               if coordinate == string.upper():
+                   return True     
+        print("Please enter a valid coordinate. For example, 'A4' or 'H8'")
+        return False
+
+    def check_direction(self, direction):
+        direction = direction.lower()
+        direction_list = ["right", "left", "down", "up"]
+        for item in direction_list:
+            if direction == item:
+                return True
+            else:
+                pass
+        print("Please enter a valid direction: right, left, up, or down.")
+        return False
+        pass
